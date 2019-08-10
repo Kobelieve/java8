27377211e270410f101e9fd0d5a385c5c1e61cfa
hutao <kobeno.1>
2019-08-10 22:39:19 +0800

    test

---
+++ src/main/java/com/kobe/practice/colon/DoubleColonDemo.java
@@ -0,0 +1,13 @@
+package com.kobe.practice.colon;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class DoubleColonDemo {
+
+    public static void main(String[] args) {
+        List<Object> list = Lists.newArrayList();
+    }
+
+}
