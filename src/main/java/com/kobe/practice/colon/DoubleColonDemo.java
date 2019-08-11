08324c3f066a79949cd52d2a1404c3dd8f3f54f8
hutao <kobeno.1>
2019-08-11 22:02:37 +0800

    DoubleColon

--- src/main/java/com/kobe/practice/colon/DoubleColonDemo.java
+++ src/main/java/com/kobe/practice/colon/DoubleColonDemo.java
@@ -1,22 +1,35 @@
 package com.kobe.practice.colon;
 
 import com.google.common.collect.Lists;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * 测试jdk1.8的双冒号用法
  */
 public class DoubleColonDemo {
 
-    /**
-     * 类名::方法名
-     *
-     * @param args
-     */
+    public static void toUpperCase(String str) {
+        System.out.println(str.toUpperCase());
+    }
+
+    public void toUpperCaseWrap(String str) {
+        System.out.println(str.toUpperCase());
+    }
+
     public static void main(String[] args) {
-        List<Object> list = Lists.newArrayList(1,2,3);
-        list.forEach(System.out::println);
+
+
+        List<String> list = Lists.newArrayList("a", "b", "c");
+
+        Consumer<String> consumer = DoubleColonDemo::toUpperCase;
+        list.forEach(consumer);
+
+        list.forEach(new DoubleColonDemo()::toUpperCaseWrap);
     }
 
 }
